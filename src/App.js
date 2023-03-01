8f2873e2375773d67016afaafb044de51b95e1ea
normanlikescats <normankwok97@gmail.com>
2023-03-01 17:09:41 +0800

    add file upload and display capability

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage, database } from "./firebase";
 import logo from "./logo.png";
 import "./App.css";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const DB_IMAGES_KEY = "images";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,13 +15,16 @@ class App extends React.Component {
     // Initialise empty messages array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
-      userInput:'',
+      textInput:'',
       messages: [],
+      fileInputValue: '',
+      fileUpload: null,
     };
   }
 
   componentDidMount() {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
+    // For Text Input
     // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
@@ -30,44 +35,72 @@ class App extends React.Component {
     });
   }
 
-  // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = (e) => {
+  imageSubmit=(e)=>{
     e.preventDefault();
     
+    // for image
+    const imageListRef = storageRef(storage, DB_IMAGES_KEY + "/" + this.state.fileInputValue);
+    uploadBytes(imageListRef, this.state.fileUpload).then((snapshot)=>{
+      console.log('uploaded')
+      console.log(snapshot['ref']['fullPath'])
+      getDownloadURL(snapshot.ref).then((url) => {
+        console.log(url);
+        this.writeData(url)
+        this.setState({
+          fileInputValue: '',
+          fileUpload: null,
+        })
+      })
+    })    
+  }
+
+  // Note use of array fields syntax to avoid having to manually bind this method to the class
+  writeData = (url) => {
+    //for text
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
     let currDate = new Date();
     set(newMessageRef, {
-      val: this.state.userInput,
+      val: this.state.textInput,
       dateTime: currDate.toLocaleDateString() + " " + currDate.toLocaleTimeString(),
+      url: url
     });
     this.setState({
-      userInput: '',
+      textInput: '',
     })
   };
 
   handleInput=(e)=> {
     this.setState({
-      userInput: e.target.value
+      textInput: e.target.value
+    })
+  }
+
+  handleFile=(e)=>{
+    this.setState({
+      fileUpload: e.target.files[0],
+      fileInputValue: e.target.files[0].name
     })
   }
 
   render() {
-    console.log(this.state.messages)
+    console.log(this.state.fileInputValue)
     // Convert messages in state to message JSX elements to render
     let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>{message.val.val} - {message.val.dateTime}</li>
+      <li key={message.key}>{message.val.val} - {message.val.dateTime} - <img className='post-image' src = {message.val.url} alt=''/></li>
     ));
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Enter your message to be stored in the database!
+            Enter your message to be stored in the database or upload an image!
           </p>
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <form onSubmit={this.writeData}>
-            <input type='input' value={this.state.userInput} onChange={this.handleInput}/>
+          <form onSubmit={this.imageSubmit}>
+            <input type='input' value={this.state.textInput} onChange={this.handleInput}/>
+            <input type='file' onChange={this.handleFile}/>
+            <br/>
             <input type='submit' value='Submit'/>
             <ol>{messageListItems}</ol>
           </form>
